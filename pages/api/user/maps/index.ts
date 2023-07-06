5fb91c89ecfc520d647cb9e1498147fec48d2e8e
tougenrip <batuhan.hincal02@gmail.com>
2023-07-07 01:22:41 +0300

    -trying fixed on pages/api/user/maps

--- pages/api/user/maps/index.ts
+++ pages/api/user/maps/index.ts
@@ -9,7 +9,7 @@ export default async function handler(req:NextApiRequest, res:NextApiResponse) {
       const session = await getServerSession(req, res, authOptions);
 
       // Error handling
-      if (!session?.user?.id) {
+      if (!session?.user?._id) {
         return res.status(401).json({
           error: {
             code: 'no-access',
